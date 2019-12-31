cf0a3382068aa2b749be61c73ece5e4a6186f2dc
Duncan Calvert <dcalvert@ihmc.us>
2019-12-30 23:14:41 -0600

    Add some chaotic system simulations.

--- src/examples/java/us/ihmc/scs2/examples/chaoticBall/ChaoticBallDefinition.java
+++ src/examples/java/us/ihmc/scs2/examples/chaoticBall/ChaoticBallDefinition.java
@@ -1,41 +1,40 @@
-package us.ihmc.scs2.examples.springDamperEngine;
+package us.ihmc.scs2.examples.chaoticBall;
 
-import us.ihmc.euclid.Axis;
+import us.ihmc.euclid.geometry.Pose3D;
 import us.ihmc.euclid.transform.RigidBodyTransform;
-import us.ihmc.scs2.definition.geometry.BoxGeometryDefinition;
-import us.ihmc.scs2.definition.robot.PrismaticJointDefinition;
+import us.ihmc.scs2.definition.geometry.SphereGeometryDefinition;
 import us.ihmc.scs2.definition.robot.RigidBodyDefinition;
 import us.ihmc.scs2.definition.robot.RobotDefinition;
+import us.ihmc.scs2.definition.robot.SixDoFJointDefinition;
 import us.ihmc.scs2.definition.robot.interfaces.RobotInitialStateProvider;
 import us.ihmc.scs2.definition.state.OneDoFJointState;
+import us.ihmc.scs2.definition.state.SixDoFJointState;
 import us.ihmc.scs2.definition.state.interfaces.JointStateReadOnly;
 import us.ihmc.scs2.definition.visual.ColorDefinition;
 import us.ihmc.scs2.definition.visual.VisualDefinition;
-import us.ihmc.scs2.definition.visual.VisualDefinition.MaterialDefinition;
 
 import java.util.Random;
 
-public class SpringDamperDefinition extends RobotDefinition implements RobotInitialStateProvider
+public class ChaoticBallDefinition extends RobotDefinition implements RobotInitialStateProvider
 {
-   public SpringDamperDefinition()
+   public ChaoticBallDefinition()
    {
-      super("springDamper");
+      super("chaoticBall");
 
       RigidBodyDefinition block = new RigidBodyDefinition("mass");
       block.setMass(1.0);
 
-      BoxGeometryDefinition geometryDefinition = new BoxGeometryDefinition(0.5, 0.5, 0.5);
+      SphereGeometryDefinition geometryDefinition = new SphereGeometryDefinition(0.1);
       RigidBodyTransform visualPose = new RigidBodyTransform();
       visualPose.setTranslationZ(0.0);
-      MaterialDefinition materialDefinition = new MaterialDefinition(new ColorDefinition(new Random().nextInt()));
+      VisualDefinition.MaterialDefinition materialDefinition = new VisualDefinition.MaterialDefinition(new ColorDefinition(new Random().nextInt()));
       block.addVisualDefinition(new VisualDefinition(visualPose, geometryDefinition, materialDefinition));
 
-      PrismaticJointDefinition slider = new PrismaticJointDefinition("slider");
-      slider.setSuccessor(block);
-      slider.getAxis().set(Axis.X);
+      SixDoFJointDefinition sixDoF = new SixDoFJointDefinition("sixDof");
+      sixDoF.setSuccessor(block);
 
       RigidBodyDefinition elevator = new RigidBodyDefinition("elevator");
-      elevator.getChildrenJoints().add(slider);
+      elevator.getChildrenJoints().add(sixDoF);
 
       setRootBodyDefinition(elevator);
    }
@@ -43,8 +42,10 @@ public class SpringDamperDefinition extends RobotDefinition implements RobotInit
    @Override
    public JointStateReadOnly getInitialJointState(String jointName)
    {
-      OneDoFJointState prismaticJointState = new OneDoFJointState();
-      prismaticJointState.setConfiguration(1.5);
-      return prismaticJointState;
+      SixDoFJointState jointState = new SixDoFJointState();
+      Pose3D pose = new Pose3D();
+      pose.setPosition(1.0, 1.0, 1.0);
+      jointState.setConfiguration(pose);
+      return jointState;
    }
 }
--- src/examples/java/us/ihmc/scs2/examples/springDamperEngine/SimplePhysicsPlugin.java
+++ src/examples/java/us/ihmc/scs2/examples/springDamperEngine/SimplePhysicsPlugin.java
@@ -1,5 +1,7 @@
 package us.ihmc.scs2.examples.springDamperEngine;
 
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.mecano.multiBodySystem.interfaces.MultiBodySystemBasics;
 import us.ihmc.mecano.yoVariables.multiBodySystem.YoPrismaticJoint;
@@ -19,26 +21,60 @@ public class SimplePhysicsPlugin implements RobotPhysicsEnginePlugin
    private double K = 50.0;
    private double b = 1.0;
 
+   private double a = 1.0;
+
+   private DenseMatrix64F A = new DenseMatrix64F(2, 2);
+   private DenseMatrix64F B = new DenseMatrix64F(2, 2);
+   private DenseMatrix64F x = new DenseMatrix64F(2, 1);
+   private DenseMatrix64F u = new DenseMatrix64F(2, 1);
+   private DenseMatrix64F xd = new DenseMatrix64F(2, 1);
+
+   // temp
+   DenseMatrix64F left = new DenseMatrix64F(2, 1);
+   DenseMatrix64F right = new DenseMatrix64F(2, 1);
+
+   double t = 0.0;
+
    @Override
    public void doScience(double dt, Vector3DReadOnly gravity)
    {
+      t += dt;
       YoPrismaticJoint joint = (YoPrismaticJoint) system.getJointsToConsider().get(0);
-
       double m = joint.getSuccessor().getInertia().getMass();
-
-      double q = joint.getQ();
+      double rawQ = joint.getQ();
       double qd = joint.getQd();
       double qdd = joint.getQdd();
+      double q = springRest - rawQ;
 
-      double depression = springRest - joint.getQ();
+      x.set(0, 0, q);
+      x.set(1, 0, qd);
+      A.set(0, 0, 0.0);
+      A.set(0, 1, 1.0);
+      A.set(1, 0, - K / m);
+      A.set(1, 1, - b / m);
 
-      double depression2 = depression + qd * dt;
+      u.set(0, 0, 1.0);
+      u.set(1, 0, Math.cos(2.0 * Math.PI * t));
+      B.set(0, 0, 0.0);
+      B.set(0, 1, 0.0);
+      B.set(1, 0, 0.0);
+      B.set(1, 1, a);
+
+      CommonOps.mult(A, x, left);
+      CommonOps.mult(B, u, right);
+
+      CommonOps.add(left, right, xd);
+
+      //      double qd2 = xd.get(0, 0);
+      double qdd2 = xd.get(1, 0);
+
+      // euler integration
       double qd2 =  qd + qdd * dt;
-      double qdd2 = - K * depression2 / m - b * qd2 / m;
+      double q2 = q + qd2 * dt;
 
-      double q2 = springRest - depression2;
+      double rawQ2 = springRest - q2;
 
-      joint.setQ(q2);
+      joint.setQ(rawQ2);
       joint.setQd(qd2);
       joint.setQdd(qdd2);
    }
