b25a1a7323208da4981115f51a6b3e19ce0f7dd7
Sylvain Bertrand <sbertrand@ihmc.us>
2020-07-12 15:35:42 -0500

    Missed some dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,9 +53,9 @@ sessionVisualizerDependencies {
    compile("us.ihmc:ihmc-javafx-toolkit:0.15.0") {
       exclude(group="us.ihmc", module="jassimp")
    }
-   api("us.ihmc:ihmc-graphics-description:0.15.0")
+   api("us.ihmc:ihmc-graphics-description:0.18.0")
    api("us.ihmc:ihmc-video-codecs:2.1.5")
-   api("us.ihmc:ihmc-robot-data-logger:0.16.0")
+   api("us.ihmc:ihmc-robot-data-logger:0.19.0")
    api("us.ihmc:svgloader:0.0")
    api("us.ihmc:ihmc-javafx-extensions:0.0")
 
