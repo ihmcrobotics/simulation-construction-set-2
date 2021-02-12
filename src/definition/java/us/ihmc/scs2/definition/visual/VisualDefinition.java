30160cc8ccb4c5af73b674251659d6c0970c0ee5
Sylvain Bertrand <sbertrand@ihmc.us>
2021-02-11 21:14:28 -0600

    Fixed the ellipsoid sim.

--- src/definition/java/us/ihmc/scs2/definition/visual/VisualDefinition.java
+++ src/definition/java/us/ihmc/scs2/definition/visual/VisualDefinition.java
@@ -1,6 +1,7 @@
 package us.ihmc.scs2.definition.visual;
 
 import us.ihmc.euclid.transform.AffineTransform;
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
 import us.ihmc.scs2.definition.geometry.GeometryDefinition;
 
 public class VisualDefinition
@@ -20,6 +21,11 @@ public class VisualDefinition
       this.materialDefinition = materialDefinition;
    }
 
+   public VisualDefinition(RigidBodyTransformReadOnly originPose, GeometryDefinition geometryDefinition, MaterialDefinition materialDefinition)
+   {
+      this(new AffineTransform(originPose), geometryDefinition, materialDefinition);
+   }
+
    public VisualDefinition(AffineTransform originPose, GeometryDefinition geometryDefinition, MaterialDefinition materialDefinition)
    {
       this.originPose = originPose;
@@ -30,6 +36,11 @@ public class VisualDefinition
    public void setName(String name)
    {
       this.name = name;
+   }
+
+   public void setOriginPose(RigidBodyTransformReadOnly originPose)
+   {
+      this.originPose = new AffineTransform(originPose);
    }
 
    public void setOriginPose(AffineTransform originPose)
--- src/examples/java/us/ihmc/scs2/examples/simulations/EllipsoidCylinderSimulation.java
+++ src/examples/java/us/ihmc/scs2/examples/simulations/EllipsoidCylinderSimulation.java
@@ -1,8 +1,9 @@
 package us.ihmc.scs2.examples.simulations;
 
-import us.ihmc.euclid.geometry.Pose3D;
-import us.ihmc.scs2.definition.robot.interfaces.RobotInitialStateProvider;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.yawPitchRoll.YawPitchRoll;
 import us.ihmc.scs2.definition.state.SixDoFJointState;
+import us.ihmc.scs2.definition.state.interfaces.JointStateBasics;
 import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizer;
 import us.ihmc.scs2.simulation.SimulationSession;
 
@@ -11,26 +12,13 @@ public class EllipsoidCylinderSimulation
    public static void main(String[] args)
    {
       EllipsoidRobotSimulation definition = new EllipsoidRobotSimulation();
-      RobotInitialStateProvider robotInitialStateProvider = robotInitialStateProvider(definition);
-      definition.setInitialStateProvider(robotInitialStateProvider);
+      JointStateBasics initialJointState = new SixDoFJointState(new YawPitchRoll(0, 0, 0.6), new Point3D(0, 0, 1.0));
+      definition.getRootJointDefinitions().get(0).setInitialJointState(initialJointState);
 
       SimulationSession simulationCore = new SimulationSession();
       simulationCore.addRobot(definition);
       simulationCore.addTerrainObject(new SlopeGroundDefinition());
 
       SessionVisualizer.startSessionVisualizer(simulationCore);
-   }
-
-   private static RobotInitialStateProvider robotInitialStateProvider(EllipsoidRobotSimulation definition)
-   {
-      return jointName ->
-      {
-         if (!jointName.equals(definition.getRootJointName()))
-            return null;
-
-         SixDoFJointState jointState = new SixDoFJointState();
-         jointState.setConfiguration(new Pose3D(0.0, 0.0, 1.0, 0.0, 0.0, 0.6));
-         return jointState;
-      };
    }
 }
