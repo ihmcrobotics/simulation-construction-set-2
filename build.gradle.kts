96093c0c2aaade4dcea5a6bcdd800d7e538cc4f2
Sylvain Bertrand <sbertrand@ihmc.us>
2021-03-16 21:33:53 -0500

    Rolled back java 9+ dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,13 +60,6 @@ sessionVisualizerJfxDependencies {
    api(ihmc.sourceSetProject("session"))
    api(ihmc.sourceSetProject("session-visualizer"))
 
-   var javaFXVersion = "15.0.1"
-   api(ihmc.javaFXModule("base", javaFXVersion))
-   api(ihmc.javaFXModule("controls", javaFXVersion))
-   api(ihmc.javaFXModule("graphics", javaFXVersion))
-   api(ihmc.javaFXModule("fxml", javaFXVersion))
-   api(ihmc.javaFXModule("swing", javaFXVersion))
-
    compile("us.ihmc:ihmc-javafx-toolkit:0.19.3") {
       exclude(group="us.ihmc", module="jassimp")
    }
@@ -79,13 +72,13 @@ sessionVisualizerJfxDependencies {
    api("org.reflections:reflections:0.9.11")
 
    // JavaFX extensions
-   api("org.controlsfx:controlsfx:11.0.1")
+   api("org.controlsfx:controlsfx:8.40.18")
    api("de.jensd:fontawesomefx-commons:9.1.2")
    api("de.jensd:fontawesomefx-octicons:4.3.0-9.1.2")
    api("de.jensd:fontawesomefx-materialicons:2.2.0-9.1.2")
    api("de.jensd:fontawesomefx-materialdesignfont:2.0.26-9.1.2")
    api("de.jensd:fontawesomefx-fontawesome:4.7.0-9.1.2")
-   api("com.jfoenix:jfoenix:9.0.10")
+   api("com.jfoenix:jfoenix:8.0.10")
    api("org.apache.commons:commons-text:1.9")
 
    api(fileTree(mapOf("dir" to "src/session-visualizer-jfx/libs/JavaFXModelImporters", "include" to "*.jar")))
