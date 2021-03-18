245e2682d9b9de4036a2b7bf5bd83a45d538bd89
Sylvain Bertrand <sbertrand@ihmc.us>
2021-03-17 23:09:48 -0500

    Added lazy update for the legend.

--- src/session-visualizer-jfx/java/us/ihmc/scs2/sessionVisualizer/jfx/controllers/chart/YoNumberSeries.java
+++ src/session-visualizer-jfx/java/us/ihmc/scs2/sessionVisualizer/jfx/controllers/chart/YoNumberSeries.java
@@ -1,27 +1,39 @@
 package us.ihmc.scs2.sessionVisualizer.jfx.controllers.chart;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import us.ihmc.scs2.sessionVisualizer.jfx.charts.NumberSeries;
 import us.ihmc.scs2.sessionVisualizer.jfx.tools.ChartTools;
+import us.ihmc.yoVariables.listener.YoVariableChangedListener;
 import us.ihmc.yoVariables.variable.YoVariable;
 
 public class YoNumberSeries extends NumberSeries
 {
    private final YoVariable yoVariable;
+   private final AtomicBoolean dirty = new AtomicBoolean(true);
+   private final YoVariableChangedListener dirtyListener = v -> dirty.set(true);
 
    public YoNumberSeries(YoVariable yoVariable)
    {
       super(yoVariable.getName());
 
       this.yoVariable = yoVariable;
+      yoVariable.addListener(dirtyListener);
    }
 
    public void updateLegend()
    {
-      setCurrentValue(ChartTools.defaultYoVariableValueFormatter(yoVariable));
+      if (dirty.getAndSet(false))
+         setCurrentValue(ChartTools.defaultYoVariableValueFormatter(yoVariable));
    }
 
    public YoVariable getYoVariable()
    {
       return yoVariable;
+   }
+
+   public void close()
+   {
+      yoVariable.removeListener(dirtyListener);
    }
 }
