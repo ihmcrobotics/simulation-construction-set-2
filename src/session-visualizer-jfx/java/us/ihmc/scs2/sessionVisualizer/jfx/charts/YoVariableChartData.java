1936f8cb828739588047ab575b32240c5f152b54
Sylvain Bertrand <sbertrand@ihmc.us>
2021-03-21 11:19:45 -0500

    Fixed behavior of buffer sample request when changing buffer size.

--- src/session-visualizer-jfx/java/us/ihmc/scs2/sessionVisualizer/jfx/charts/YoVariableChartData.java
+++ src/session-visualizer-jfx/java/us/ihmc/scs2/sessionVisualizer/jfx/charts/YoVariableChartData.java
@@ -94,6 +94,10 @@ public class YoVariableChartData
          { // First time requesting data.
             linkedYoVariable.requestEntireBuffer();
          }
+         else if (currentBufferProperties.getSize() != lastProperties.getSize())
+         { // Buffer was either resized or cropped, data has been shifted around, need to get a complete update.
+            linkedYoVariable.requestEntireBuffer();
+         }
          else if (lastSessionModeStatus != SessionMode.RUNNING && currentSessionMode.get() == SessionMode.RUNNING)
          { // The session just start running, need to ensure we have all the data since it started running.
             linkedYoVariable.requestActiveBufferOnly();
@@ -104,10 +108,6 @@ public class YoVariableChartData
                linkedYoVariable.requestActiveBufferOnly();
             else
                linkedYoVariable.requestBufferStartingFrom(lastUpdateEndIndex);
-         }
-         else if (currentBufferProperties.getSize() != lastProperties.getSize())
-         { // Buffer was either resized or cropped, data has been shifted around, need to get a complete update.
-            linkedYoVariable.requestEntireBuffer();
          }
          else if (currentBufferProperties.getInPoint() != lastProperties.getInPoint() && currentBufferProperties.getOutPoint() != lastProperties.getOutPoint())
          { // When cropping without actually changing the size of the buffer, the data is still being shifted around.
