09664b323eb71af8d6c16fc957f26b71827fb43b
Sylvain Bertrand <sbertrand@ihmc.us>
2021-11-03 18:05:08 -0500

    Added constructors for convenience.

--- scs2-definition/src/main/java/us/ihmc/scs2/definition/robot/WrenchSensorDefinition.java
+++ scs2-definition/src/main/java/us/ihmc/scs2/definition/robot/WrenchSensorDefinition.java
@@ -1,9 +1,22 @@
 package us.ihmc.scs2.definition.robot;
 
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
+
 public class WrenchSensorDefinition extends SensorDefinition
 {
    public WrenchSensorDefinition()
    {
+   }
+
+   public WrenchSensorDefinition(String name, Tuple3DReadOnly offsetFromJoint)
+   {
+      super(name, offsetFromJoint);
+   }
+
+   public WrenchSensorDefinition(String name, RigidBodyTransformReadOnly transformToJoint)
+   {
+      super(name, transformToJoint);
    }
 
    public WrenchSensorDefinition(WrenchSensorDefinition other)
