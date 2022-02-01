52ec3a3b377cbe492a66d28efd5d1460ad01d576
Sylvain Bertrand <sbertrand@ihmc.us>
2022-02-01 09:02:41 -0600

    :arrow_up: euclid 0.17.1.

--- scs2-session-visualizer-jfx/build.gradle.kts
+++ scs2-session-visualizer-jfx/build.gradle.kts
@@ -22,9 +22,9 @@ mainDependencies {
       exclude(group="us.ihmc", module="euclid-shape")
       exclude(group="us.ihmc", module="euclid-frame")
    }
-   api("us.ihmc:euclid:0.17.0")
-   api("us.ihmc:euclid-shape:0.17.0")
-   api("us.ihmc:euclid-frame:0.17.0")
+   api("us.ihmc:euclid:0.17.1")
+   api("us.ihmc:euclid-shape:0.17.1")
+   api("us.ihmc:euclid-frame:0.17.1")
    api("us.ihmc:ihmc-graphics-description:0.19.4")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:ihmc-robot-data-logger:0.20.9")
