c8b180dfa1797ce7db31247178d36d6d575cf2d3
Sylvain Bertrand <sbertrand@ihmc.org>
2022-02-07 17:24:44 -0600

    :arrow_up: ihmc-robot-data-logger 0.22.1

--- scs2-session-visualizer-jfx/build.gradle.kts
+++ scs2-session-visualizer-jfx/build.gradle.kts
@@ -27,7 +27,7 @@ mainDependencies {
    api("us.ihmc:euclid-frame:0.17.1")
    api("us.ihmc:ihmc-graphics-description:0.19.4")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
-   api("us.ihmc:ihmc-robot-data-logger:0.20.9")
+   api("us.ihmc:ihmc-robot-data-logger:0.22.1")
    api("us.ihmc:svgloader:0.0")
    api("us.ihmc:ihmc-javafx-extensions:0.0.3")
 
