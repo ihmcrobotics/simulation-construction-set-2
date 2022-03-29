ccbf3c21d25be778bd14e55c26880bafa4a20f94
Sylvain Bertrand <sbertrand@ihmc.org>
2022-03-28 19:40:30 -0500

    Added documentation

--- scs2-session/src/main/java/us/ihmc/scs2/session/YoTimer.java
+++ scs2-session/src/main/java/us/ihmc/scs2/session/YoTimer.java
@@ -5,7 +5,12 @@ import java.util.concurrent.TimeUnit;
 import us.ihmc.yoVariables.exceptions.IllegalOperationException;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
+import us.ihmc.yoVariables.variable.YoVariable;
 
+/**
+ * This class can be used to measure execution time of some code. The time variable is backed by a
+ * {@link YoVariable} allowing to visualize the timer in the SCS2 GUI.
+ */
 public class YoTimer
 {
    private final YoDouble timer;
@@ -14,6 +19,13 @@ public class YoTimer
    private long start = -1;
    private long lastUpdateTime = -1;
 
+   /**
+    * Creates a new instance of a timer.
+    * 
+    * @param namePrefix the name prefix used for creating the time variable.
+    * @param timerUnit  the unit to use for storing time measurements.
+    * @param registry   the registry to attach the internal variable to.
+    */
    public YoTimer(String namePrefix, TimeUnit timerUnit, YoRegistry registry)
    {
       timer = new YoDouble(namePrefix + "[" + timeUnitSuffix(timerUnit) + "]", registry);
@@ -66,31 +78,81 @@ public class YoTimer
       }
    }
 
+   /**
+    * Marks the start of a new measurement.
+    * <p>
+    * Uses {@link System#nanoTime()} to get the current time.
+    * </p>
+    * <p>
+    * The measurement will be the time elapsed from start to stop.
+    * </p>
+    */
    public void start()
    {
-      start = System.nanoTime();
+      start(System.nanoTime());
    }
 
+   /**
+    * Marks the start of a new measurement.
+    * <p>
+    * The measurement will be the time elapsed from start to stop.
+    * </p>
+    * 
+    * @param now      the current time.
+    * @param timeUnit the unit for {@code now}.
+    */
    public void start(long now, TimeUnit timeUnit)
    {
       start(timeUnit.toNanos(now));
    }
 
+   /**
+    * Marks the start of a new measurement.
+    * <p>
+    * The measurement will be the time elapsed from start to stop.
+    * </p>
+    * 
+    * @param nowNanosec the current time in nanoseconds.
+    */
    public void start(long nowNanosec)
    {
       start = nowNanosec;
    }
 
+   /**
+    * Marks the end of the current measurement and update the timer variable. The measurement is the
+    * time elapsed since start was called.
+    * <p>
+    * Uses {@link System#nanoTime()} to get the current time.
+    * </p>
+    * 
+    * @throws IllegalOperationException if {@link #start} was not called last.
+    */
    public void stop()
    {
       stop(System.nanoTime());
    }
 
+   /**
+    * Marks the end of the current measurement and update the timer variable. The measurement is the
+    * time elapsed since start was called.
+    * 
+    * @param now      the current time.
+    * @param timeUnit the unit for {@code now}.
+    * @throws IllegalOperationException if {@link #start} was not called last.
+    */
    public void stop(long now, TimeUnit timeUnit)
    {
       stop(timeUnit.toNanos(now));
    }
 
+   /**
+    * Marks the end of the current measurement and update the timer variable. The measurement is the
+    * time elapsed since start was called.
+    * 
+    * @param nowNanosec the current time in nanoseconds.
+    * @throws IllegalOperationException if {@link #start} was not called last.
+    */
    public void stop(long nowNanosec)
    {
       if (start == -1)
@@ -100,22 +162,42 @@ public class YoTimer
       start = -1;
    }
 
+   /**
+    * Clears the internal data but keeps the timer value.
+    */
    public void reset()
    {
       start = -1;
       lastUpdateTime = -1;
    }
 
+   /**
+    * Updates the time measurement as the time elapsed since the last call to update.
+    * <p>
+    * Uses {@link System#nanoTime()} to get the current time.
+    * </p>
+    */
    public void update()
    {
       update(System.nanoTime());
    }
 
+   /**
+    * Updates the time measurement as the time elapsed since the last call to update.
+    * 
+    * @param now      the current time.
+    * @param timeUnit the unit for {@code now}.
+    */
    public void update(long now, TimeUnit timeUnit)
    {
       update(timeUnit.toNanos(now));
    }
 
+   /**
+    * Updates the time measurement as the time elapsed since the last call to update.
+    * 
+    * @param nowNanosec the current time in nanoseconds.
+    */
    public void update(long nowNanosec)
    {
       if (lastUpdateTime > -1)
@@ -126,6 +208,11 @@ public class YoTimer
       lastUpdateTime = nowNanosec;
    }
 
+   /**
+    * Gets the timer variable that holds onto the last time measurement.
+    * 
+    * @return the timer variable.
+    */
    public YoDouble getTimer()
    {
       return timer;
