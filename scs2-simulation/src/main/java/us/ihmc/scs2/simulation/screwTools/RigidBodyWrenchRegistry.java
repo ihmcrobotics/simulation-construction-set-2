1391d361eb6fabe1326ad27bf81f46edad7cb053
Tammy Vander Hey <tvanderhye@ihmc.org>
2022-04-28 15:45:16 -0500

    Added wrenchCache and set each wrench to zero when the reset method is called.

--- scs2-simulation/src/main/java/us/ihmc/scs2/simulation/screwTools/RigidBodyWrenchRegistry.java
+++ scs2-simulation/src/main/java/us/ihmc/scs2/simulation/screwTools/RigidBodyWrenchRegistry.java
@@ -1,6 +1,8 @@
 package us.ihmc.scs2.simulation.screwTools;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -11,7 +13,13 @@ import us.ihmc.mecano.spatial.interfaces.WrenchReadOnly;
 public class RigidBodyWrenchRegistry implements Function<RigidBodyReadOnly, WrenchReadOnly>
 {
    private final Map<RigidBodyReadOnly, Wrench> rigidBodyWrenchMap = new HashMap<>();
-   private final Function<RigidBodyReadOnly, Wrench> wrenchFactory = body -> new Wrench(body.getBodyFixedFrame(), body.getBodyFixedFrame());
+   private final List<Wrench> wrenchCache = new ArrayList<>();
+   private final Function<RigidBodyReadOnly, Wrench> wrenchFactory = body -> {
+      
+      Wrench wrench = new Wrench(body.getBodyFixedFrame(), body.getBodyFixedFrame());
+      wrenchCache.add(wrench);
+      return wrench;
+   };
 
    public RigidBodyWrenchRegistry()
    {
@@ -19,7 +27,10 @@ public class RigidBodyWrenchRegistry implements Function<RigidBodyReadOnly, Wren
 
    public void reset()
    {
-      rigidBodyWrenchMap.clear();
+      for (int i = 0; i < wrenchCache.size(); i++)
+      {
+         wrenchCache.get(i).setToZero();
+      }
    }
 
    public void addWrench(RigidBodyReadOnly target, WrenchReadOnly wrenchToAdd)
