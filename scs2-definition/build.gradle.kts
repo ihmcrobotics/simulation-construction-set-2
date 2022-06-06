539500647040b3588a2ec08d8b825509ceb4e570
Sylvain Bertrand <sbertrand@ihmc.org>
2022-06-06 16:34:47 -0500

    :arrow_up: ihmc-yovariables 0.9.13

--- scs2-definition/build.gradle.kts
+++ scs2-definition/build.gradle.kts
@@ -16,7 +16,7 @@ mainDependencies {
    api("us.ihmc:euclid-shape:0.17.2")
    api("us.ihmc:euclid-frame:0.17.2")
    api("us.ihmc:ihmc-commons:0.30.6")
-   api("us.ihmc:ihmc-yovariables:0.9.12")
+   api("us.ihmc:ihmc-yovariables:0.9.13")
    api("us.ihmc:mecano:0.11.0")
 }
 
