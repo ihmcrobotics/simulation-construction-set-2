a86838e89aa680e29bf84808482424e891c3bfaf
Duncan Calvert <dcalvert@ihmc.us>
2022-07-25 16:13:27 -0500

    WIP: Start trying to measure realtime rate.

--- scs2-examples/src/main/java/us/ihmc/scs2/examples/simulations/bullet/MobileBulletSimulation.java
+++ scs2-examples/src/main/java/us/ihmc/scs2/examples/simulations/bullet/MobileBulletSimulation.java
@@ -1,15 +1,18 @@
 package us.ihmc.scs2.examples.simulations.bullet;
 
+import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizer;
 import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizerControls;
 import us.ihmc.scs2.simulation.SimulationSession;
 import us.ihmc.scs2.simulation.bullet.physicsEngine.BulletPhysicsEngineFactory;
 import us.ihmc.scs2.simulation.bullet.physicsEngine.parameters.BulletMultiBodyJointParameters;
 import us.ihmc.scs2.simulation.bullet.physicsEngine.parameters.BulletMultiBodyParameters;
+import us.ihmc.yoVariables.variable.YoDouble;
 
 public class MobileBulletSimulation
 {
    private static final boolean VISUALIZE_WITH_DEBUG_DRAWING = false;
+   private static YoDouble realtimeRate;
 
    public static void main(String[] args)
    {
@@ -22,6 +25,10 @@ public class MobileBulletSimulation
       SimulationSession simulationSession = new SimulationSession(BulletPhysicsEngineFactory.newBulletPhysicsEngineFactory(parameters, jointParameters));
       simulationSession.addRobot(definition);
 
+      realtimeRate = new YoDouble("realtimeRate", simulationSession.getRootRegistry());
+
+
+
       if (VISUALIZE_WITH_DEBUG_DRAWING)
       {
          SessionVisualizer sessionVisualizer = BulletExampleSimulationTools.startSessionVisualizerWithDebugDrawing(simulationSession);
@@ -32,6 +39,12 @@ public class MobileBulletSimulation
       {
          SessionVisualizerControls sessionVisualizerControls = SessionVisualizer.startSessionVisualizer(simulationSession, null);
          sessionVisualizerControls.setCameraFocusPosition(0.0, 0.0, 0.7);
+
+//         sessionVisualizerControls.
+
+//         ThreadTools.
+
+
       }
    }
 }
