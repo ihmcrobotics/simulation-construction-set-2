7478b542e8885286ae8865c1c42efa64ebcd168e
Sylvain Bertrand <sbertrand@ihmc.org>
2022-08-01 18:03:35 -0500

    The joint builder was missing the spherical joint.

--- scs2-simulation/src/main/java/us/ihmc/scs2/simulation/robot/Robot.java
+++ scs2-simulation/src/main/java/us/ihmc/scs2/simulation/robot/Robot.java
@@ -34,6 +34,7 @@ import us.ihmc.scs2.definition.robot.RobotDefinition;
 import us.ihmc.scs2.definition.robot.RobotStateDefinition;
 import us.ihmc.scs2.definition.robot.SensorDefinition;
 import us.ihmc.scs2.definition.robot.SixDoFJointDefinition;
+import us.ihmc.scs2.definition.robot.SphericalJointDefinition;
 import us.ihmc.scs2.definition.robot.WrenchSensorDefinition;
 import us.ihmc.scs2.definition.robot.RobotStateDefinition.JointStateEntry;
 import us.ihmc.scs2.definition.state.JointState;
@@ -54,6 +55,7 @@ import us.ihmc.scs2.simulation.robot.multiBodySystem.SimPrismaticJoint;
 import us.ihmc.scs2.simulation.robot.multiBodySystem.SimRevoluteJoint;
 import us.ihmc.scs2.simulation.robot.multiBodySystem.SimRigidBody;
 import us.ihmc.scs2.simulation.robot.multiBodySystem.SimSixDoFJoint;
+import us.ihmc.scs2.simulation.robot.multiBodySystem.SimSphericalJoint;
 import us.ihmc.scs2.simulation.robot.multiBodySystem.interfaces.SimJointBasics;
 import us.ihmc.scs2.simulation.robot.multiBodySystem.interfaces.SimRigidBodyBasics;
 import us.ihmc.yoVariables.registry.YoRegistry;
@@ -374,6 +376,8 @@ public class Robot implements RobotInterface
             return new SimPrismaticJoint((PrismaticJointDefinition) definition, predecessor);
          if (definition instanceof RevoluteJointDefinition)
             return new SimRevoluteJoint((RevoluteJointDefinition) definition, predecessor);
+         if (definition instanceof SphericalJointDefinition)
+            return new SimSphericalJoint((SphericalJointDefinition) definition, predecessor);
          if (definition instanceof CrossFourBarJointDefinition)
             return new SimCrossFourBarJoint((CrossFourBarJointDefinition) definition, predecessor);
          throw new UnsupportedOperationException("Unsupported joint definition: " + definition.getClass().getSimpleName());
