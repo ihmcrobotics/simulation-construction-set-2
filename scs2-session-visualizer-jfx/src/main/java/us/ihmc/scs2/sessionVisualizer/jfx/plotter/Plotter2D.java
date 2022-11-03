5b94c5b62b30fc33cb03cb492e3923b3187742f5
Sylvain Bertrand <sbertrand@ihmc.org>
2022-11-02 23:32:11 -0500

    Do not pick on bounds for polygon

--- scs2-session-visualizer-jfx/src/main/java/us/ihmc/scs2/sessionVisualizer/jfx/plotter/Plotter2D.java
+++ scs2-session-visualizer-jfx/src/main/java/us/ihmc/scs2/sessionVisualizer/jfx/plotter/Plotter2D.java
@@ -30,6 +30,7 @@ import us.ihmc.scs2.sessionVisualizer.jfx.tools.JavaFXToEuclidConversions;
 import us.ihmc.scs2.sessionVisualizer.jfx.yoComposite.Tuple2DProperty;
 import us.ihmc.scs2.sessionVisualizer.jfx.yoGraphic.YoGraphicFXItem;
 import us.ihmc.scs2.sessionVisualizer.jfx.yoGraphic.YoGroupFX;
+import us.ihmc.scs2.sessionVisualizer.jfx.yoGraphic.YoPolygonFX2D;
 
 public class Plotter2D extends Region
 {
@@ -192,7 +193,8 @@ public class Plotter2D extends Region
                   activeTooltip.set(tooltip);
             };
             tooltip.showingProperty().addListener(tooltipShowingListener);
-            node.setPickOnBounds(true);
+            if (!(yoGraphicFX instanceof YoPolygonFX2D)) // The JavaFX bounding box for Polygon/Path is artificially over-grown
+               node.setPickOnBounds(true);
             installedTooltips.put(node, tooltip);
             Tooltip.install(node, tooltip);
          }
