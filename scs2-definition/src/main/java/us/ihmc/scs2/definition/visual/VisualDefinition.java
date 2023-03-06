62224afa86303631a81bbef05af4193f78b28fff
Sylvain Bertrand <sbertrand@ihmc.org>
2023-03-06 17:42:37 -0600

    Added some doc for VisualDefinition

--- scs2-definition/src/main/java/us/ihmc/scs2/definition/visual/VisualDefinition.java
+++ scs2-definition/src/main/java/us/ihmc/scs2/definition/visual/VisualDefinition.java
@@ -12,40 +12,176 @@ import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
 import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.scs2.definition.AffineTransformDefinition;
 import us.ihmc.scs2.definition.geometry.GeometryDefinition;
+import us.ihmc.scs2.definition.geometry.ModelFileGeometryDefinition;
+import us.ihmc.scs2.definition.geometry.Sphere3DDefinition;
 
+/**
+ * A {@code VisualDefinition} represents a template for creating 3D graphics that can be used to
+ * render a piece of the environment or a body of a robot.
+ * <p>
+ * See the helper class {@link VisualDefinitionFactory} that adds convenience methods for creating
+ * new visuals.
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ */
 public class VisualDefinition
 {
+   /** A descriptive/representative name to be associated with this visual. */
    private String name;
-   /** In parent after joint frame. */
+   /**
+    * The pose of the geometry.
+    * <p>
+    * When this visual is used for rendering a body of a robot, the pose is relative to the frame after
+    * the parent joint of the visualized body.
+    * </p>
+    */
    private AffineTransformDefinition originPose = new AffineTransformDefinition();
+   /**
+    * The geometry of this visual. It can for instance be a primitive such as a
+    * {@link Sphere3DDefinition} or a link to a model file {@link ModelFileGeometryDefinition}.
+    */
    private GeometryDefinition geometryDefinition;
+   /**
+    * The material to apply on the geometry. It can be as simple as just a diffuse color using
+    * {@link MaterialDefinition#MaterialDefinition(ColorDefinition)} or a texture
+    * {@link MaterialDefinition#MaterialDefinition(TextureDefinition)}.
+    */
    private MaterialDefinition materialDefinition;
 
+   /**
+    * Creates an empty visual which fields need to be initialized.
+    */
    public VisualDefinition()
    {
    }
 
+   /**
+    * Creates a new visual using a "zero" pose.
+    * 
+    * @param geometryDefinition the geometry to use.
+    * @param diffuseColor       the diffuse color for creating this visual's material.
+    */
+   public VisualDefinition(GeometryDefinition geometryDefinition, ColorDefinition diffuseColor)
+   {
+      this(geometryDefinition, new MaterialDefinition(diffuseColor));
+   }
+
+   /**
+    * Creates a new visual using a "zero" pose.
+    * 
+    * @param geometryDefinition the geometry to use.
+    * @param materialDefinition the material to use.
+    */
    public VisualDefinition(GeometryDefinition geometryDefinition, MaterialDefinition materialDefinition)
    {
       this.geometryDefinition = geometryDefinition;
       this.materialDefinition = materialDefinition;
    }
 
+   /**
+    * Creates a new visual.
+    * 
+    * @param originPosition     the position of the geometry. If this is a visual for a robot body, the
+    *                           position is with respect to the parent joint frame.
+    * @param geometryDefinition the geometry to use.
+    * @param diffuseColor       the diffuse color for creating this visual's material.
+    */
+   public VisualDefinition(Tuple3DReadOnly originPosition, GeometryDefinition geometryDefinition, ColorDefinition diffuseColor)
+   {
+      this(originPosition, geometryDefinition, new MaterialDefinition(diffuseColor));
+   }
+
+   /**
+    * Creates a new visual.
+    * 
+    * @param originPosition     the position of the geometry. If this is a visual for a robot body, the
+    *                           position is with respect to the parent joint frame.
+    * @param geometryDefinition the geometry to use.
+    * @param materialDefinition the material to use.
+    */
    public VisualDefinition(Tuple3DReadOnly originPosition, GeometryDefinition geometryDefinition, MaterialDefinition materialDefinition)
    {
       this(new AffineTransform(new Quaternion(), originPosition), geometryDefinition, materialDefinition);
    }
 
+   /**
+    * Creates a new visual.
+    * 
+    * @param originPose         the pose of the geometry. If this is a visual for a robot body, the
+    *                           pose is with respect to the parent joint frame.
+    * @param geometryDefinition the geometry to use.
+    * @param diffuseColor       the diffuse color for creating this visual's material.
+    */
+   public VisualDefinition(RigidBodyTransformReadOnly originPose, GeometryDefinition geometryDefinition, ColorDefinition diffuseColor)
+   {
+      this(originPose, geometryDefinition, new MaterialDefinition(diffuseColor));
+   }
+
+   /**
+    * Creates a new visual.
+    * 
+    * @param originPose         the pose of the geometry. If this is a visual for a robot body, the
+    *                           pose is with respect to the parent joint frame.
+    * @param geometryDefinition the geometry to use.
+    * @param materialDefinition the material to use.
+    */
    public VisualDefinition(RigidBodyTransformReadOnly originPose, GeometryDefinition geometryDefinition, MaterialDefinition materialDefinition)
    {
       this(new AffineTransform(originPose), geometryDefinition, materialDefinition);
    }
 
+   /**
+    * Creates a new visual.
+    * 
+    * @param originPose         the transform (translation, rotation, scale) of the geometry. If this
+    *                           is a visual for a robot body, the transform is with respect to the
+    *                           parent joint frame.
+    * @param geometryDefinition the geometry to use.
+    * @param diffuseColor       the diffuse color for creating this visual's material.
+    */
+   public VisualDefinition(AffineTransformReadOnly originPose, GeometryDefinition geometryDefinition, ColorDefinition diffuseColor)
+   {
+      this(originPose, geometryDefinition, new MaterialDefinition(diffuseColor));
+   }
+
+   /**
+    * Creates a new visual.
+    * 
+    * @param originPose         the transform (translation, rotation, scale) of the geometry. If this
+    *                           is a visual for a robot body, the transform is with respect to the
+    *                           parent joint frame.
+    * @param geometryDefinition the geometry to use.
+    * @param materialDefinition the material to use.
+    */
    public VisualDefinition(AffineTransformReadOnly originPose, GeometryDefinition geometryDefinition, MaterialDefinition materialDefinition)
    {
       this(new AffineTransformDefinition(originPose), geometryDefinition, materialDefinition);
    }
 
+   /**
+    * Creates a new visual.
+    * 
+    * @param originPose         the transform (translation, rotation, scale) of the geometry. If this
+    *                           is a visual for a robot body, the transform is with respect to the
+    *                           parent joint frame.
+    * @param geometryDefinition the geometry to use.
+    * @param diffuseColor       the diffuse color for creating this visual's material.
+    */
+   public VisualDefinition(AffineTransformDefinition originPose, GeometryDefinition geometryDefinition, ColorDefinition diffuseColor)
+   {
+      this(originPose, geometryDefinition, new MaterialDefinition(diffuseColor));
+   }
+
+   /**
+    * Creates a new visual.
+    * 
+    * @param originPose         the transform (translation, rotation, scale) of the geometry. If this
+    *                           is a visual for a robot body, the transform is with respect to the
+    *                           parent joint frame.
+    * @param geometryDefinition the geometry to use.
+    * @param materialDefinition the material to use.
+    */
    public VisualDefinition(AffineTransformDefinition originPose, GeometryDefinition geometryDefinition, MaterialDefinition materialDefinition)
    {
       this.originPose = originPose;
@@ -53,6 +189,11 @@ public class VisualDefinition
       this.materialDefinition = materialDefinition;
    }
 
+   /**
+    * Copy constructor. This constructor performs a deep copy.
+    * 
+    * @param other the other visual to make a copy of.
+    */
    public VisualDefinition(VisualDefinition other)
    {
       name = other.name;
@@ -63,34 +204,79 @@ public class VisualDefinition
          materialDefinition = other.materialDefinition.copy();
    }
 
+   /**
+    * Sets descriptive/representative name to be associated with this visual.
+    * 
+    * @param name this visual's name.
+    */
    @XmlElement
    public void setName(String name)
    {
       this.name = name;
    }
 
+   /**
+    * Sets the pose of the geometry.
+    * <p>
+    * When this visual is used for rendering a body of a robot, the pose is relative to the frame after
+    * the parent joint of the visualized body.
+    * </p>
+    * 
+    * @param originPose the pose for the geometry.
+    */
    public void setOriginPose(RigidBodyTransformReadOnly originPose)
    {
       this.originPose = new AffineTransformDefinition(originPose);
    }
 
+   /**
+    * Sets the transform (translation, rotation, scale) of the geometry.
+    * <p>
+    * When this visual is used for rendering a body of a robot, the transform is relative to the frame
+    * after the parent joint of the visualized body.
+    * </p>
+    * 
+    * @param originPose the transform for the geometry.
+    */
    public void setOriginPose(AffineTransformReadOnly originPose)
    {
       this.originPose = new AffineTransformDefinition(originPose);
    }
 
+   /**
+    * Sets the transform (translation, rotation, scale) of the geometry.
+    * <p>
+    * When this visual is used for rendering a body of a robot, the transform is relative to the frame
+    * after the parent joint of the visualized body.
+    * </p>
+    * 
+    * @param originPose the transform for the geometry.
+    */
    @XmlElement
    public void setOriginPose(AffineTransformDefinition originPose)
    {
       this.originPose = originPose;
    }
 
+   /**
+    * Sets the geometry of this visual. It can for instance be a primitive such as a
+    * {@link Sphere3DDefinition} or a link to a model file {@link ModelFileGeometryDefinition}.
+    * 
+    * @param geometryDefinition this visual's geometry.
+    */
    @XmlElement
    public void setGeometryDefinition(GeometryDefinition geometryDefinition)
    {
       this.geometryDefinition = geometryDefinition;
    }
 
+   /**
+    * Sets the material to apply on the geometry. It can be as simple as just a diffuse color using
+    * {@link MaterialDefinition#MaterialDefinition(ColorDefinition)} or a texture
+    * {@link MaterialDefinition#MaterialDefinition(TextureDefinition)}.
+    * 
+    * @param materialDefinition the material to use for this visual.
+    */
    @XmlElement
    public void setMaterialDefinition(MaterialDefinition materialDefinition)
    {
