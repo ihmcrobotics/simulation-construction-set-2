7bdc3bf73f45a24ca21d520676356557cfe95a8c
Sylvain Bertrand <sbertrand@ihmc.org>
2023-03-21 13:16:54 -0500

    :arrow_up: ihmc-javafx-toolkit

--- scs2-bullet-simulation/build.gradle.kts
+++ scs2-bullet-simulation/build.gradle.kts
@@ -30,7 +30,7 @@ mainDependencies {
 debugDependencies {
    api(ihmc.sourceSetProject("main"))
 
-   api("us.ihmc:ihmc-javafx-toolkit:17-0.21.4") {
+   api("us.ihmc:ihmc-javafx-toolkit:17-0.22.0") {
       exclude(group="us.ihmc", module="jassimp")
       exclude(group="us.ihmc", module="euclid")
       exclude(group="us.ihmc", module="euclid-shape")
