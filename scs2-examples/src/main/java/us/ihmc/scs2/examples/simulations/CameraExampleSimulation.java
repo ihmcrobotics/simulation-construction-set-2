87cab708a3b4d2ebb2e3cab637d41c3ac24944f4
Sylvain Bertrand <sbertrand@ihmc.org>
2023-08-14 09:04:58 -0500

    temp
    Former-commit-id: cd5fbdf6f4daccadd91142b070abe9a15bcb3256

--- scs2-examples/src/main/java/us/ihmc/scs2/examples/simulations/CameraExampleSimulation.java
+++ scs2-examples/src/main/java/us/ihmc/scs2/examples/simulations/CameraExampleSimulation.java
@@ -3,6 +3,7 @@ package us.ihmc.scs2.examples.simulations;
 import us.ihmc.euclid.geometry.Pose3D;
 import us.ihmc.scs2.SimulationConstructionSet2;
 import us.ihmc.scs2.definition.state.SixDoFJointState;
+import us.ihmc.scs2.simulation.robot.Robot;
 import us.ihmc.yoVariables.variable.YoDouble;
 
 public class CameraExampleSimulation
@@ -33,7 +34,25 @@ public class CameraExampleSimulation
       cameraLongitude.set(0.5 * Math.PI);
       cameraLatitude.set(0.5 * Math.PI);
 
+      scs.addAfterPhysicsCallback(time ->
+      {
+         cameraX.set(0.25 * Math.sin(2.0 * Math.PI / 2.0) + Math.toRadians(30));
+         cameraY.set(0.25 * Math.sin(2.0 * Math.PI / 2.0) + Math.toRadians(60));
+         cameraZ.set(0.25 * Math.sin(2.0 * Math.PI / 2.0) + Math.toRadians(90));
+
+         cameraDistance.set(0.25 * (1.0 + Math.sin(2.0 * Math.PI / 2.0) + Math.toRadians(30)));
+         cameraLongitude.set(0.25 * Math.sin(2.0 * Math.PI / 2.0) + Math.toRadians(60));
+         cameraLatitude.set(0.25 * Math.sin(2.0 * Math.PI / 2.0) + Math.toRadians(90));
+      });
+
+      scs.requestCameraRigidBodyTracking(definition.getName(), definition.getFloatingRootJointDefinition().getSuccessor().getName());
+//      scs.requestCameraPositionTracking(cameraX, cameraY, cameraZ);
+//      scs.requestCameraOrbitTracking(cameraDistance, cameraLongitude, cameraLatitude);
+      scs.requestCameraLevelOrbitTracking(cameraDistance, cameraLongitude, cameraZ);
+      
+      
       scs.initializeBufferSize(100000);
+      scs.setRealTimeRateSimulation(true);
       scs.start(true, false, false);
    }
 }
