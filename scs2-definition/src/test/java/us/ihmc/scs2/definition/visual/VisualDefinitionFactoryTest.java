9f6b5b0510f46e18173bad68ce7a88d2ecc8f225
Achintya Mohan <amohan@ihmc.org>
2023-09-25 16:23:47 -0400

    Add test for restoring current transform after adding arrow

---
+++ scs2-definition/src/test/java/us/ihmc/scs2/definition/visual/VisualDefinitionFactoryTest.java
@@ -0,0 +1,39 @@
+package us.ihmc.scs2.definition.visual;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import us.ihmc.euclid.transform.AffineTransform;
+import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
+import us.ihmc.euclid.tools.EuclidCoreTestTools;
+
+public class VisualDefinitionFactoryTest
+{
+   static final double EPS = 1e-10;
+   @Test
+   public void testAddArrowPreviousTransform()
+   {
+      Random random = new Random(23943L);
+      VisualDefinitionFactory v = new VisualDefinitionFactory();
+
+      // Set a non-zero initial transform
+      RigidBodyTransformReadOnly initialTransform = EuclidCoreRandomTools.nextRigidBodyTransform(random);
+      v.appendTransform(initialTransform);
+      v.saveCurrentTransform();
+
+      RigidBodyTransformReadOnly t1 = EuclidCoreRandomTools.nextRigidBodyTransform(random);
+      v.appendTransform(t1);
+      v.addArrow(EuclidCoreRandomTools.nextAxis3D(random), random.nextDouble());
+
+      // reset to the previous saved transform, i.e. initialTransform in this case
+      v.resetCurrentTransform();
+
+      EuclidCoreTestTools.assertGeometricallyEquals(new AffineTransform(initialTransform), v.getCurrentTransform(), EPS);
+   }
+}
+
