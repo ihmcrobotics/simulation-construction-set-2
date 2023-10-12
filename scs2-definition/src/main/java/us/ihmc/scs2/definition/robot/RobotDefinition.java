22ee01818f88987aaab9a2a8d13121792fc4ac31
Sylvain Bertrand <sbertrand@ihmc.org>
2023-10-12 17:37:53 -0500

    Forgot to save the CoM offset.

--- scs2-definition/src/main/java/us/ihmc/scs2/definition/robot/RobotDefinition.java
+++ scs2-definition/src/main/java/us/ihmc/scs2/definition/robot/RobotDefinition.java
@@ -16,6 +16,7 @@ import us.ihmc.euclid.orientation.interfaces.Orientation3DReadOnly;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.tools.EuclidHashCodeTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformBasics;
 import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.mecano.multiBodySystem.interfaces.JointBasics;
@@ -510,10 +511,11 @@ public class RobotDefinition
       if (jointDefinition instanceof OneDoFJointDefinition)
          jointRotation.transform(((OneDoFJointDefinition) jointDefinition).getAxis());
       RigidBodyDefinition linkDefinition = jointDefinition.getSuccessor();
-      RigidBodyTransform inertiaPose = new RigidBodyTransform(linkDefinition.getInertiaPose());
+      RigidBodyTransformBasics inertiaPose = new RigidBodyTransform(linkDefinition.getInertiaPose());
       inertiaPose.prependOrientation(jointRotation);
       inertiaPose.transform(linkDefinition.getMomentOfInertia());
       linkDefinition.getInertiaPose().getRotation().setToZero();
+      linkDefinition.getInertiaPose().getTranslation().set(inertiaPose.getTranslation());
 
       for (KinematicPointDefinition kinematicPointDefinition : jointDefinition.getKinematicPointDefinitions())
          kinematicPointDefinition.getTransformToParent().prependOrientation(jointRotation);
