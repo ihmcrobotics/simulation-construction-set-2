194a814294397c33f8349c410ce5bf741e98848d
Sylvain Bertrand <sbertrand@ihmc.org>
2023-10-16 09:18:16 -0500

    :arrow_up: yovariables 0.9.19

--- scs2-bullet-simulation/build.gradle.kts
+++ scs2-bullet-simulation/build.gradle.kts
@@ -20,7 +20,7 @@ mainDependencies {
    api("us.ihmc:scs2-session:source")
    api("us.ihmc:euclid-frame-shape:0.20.0")
    api("us.ihmc:ihmc-messager:0.2.0")
-   api("us.ihmc:ihmc-yovariables:0.9.18")
+   api("us.ihmc:ihmc-yovariables:0.9.19")
    api("us.ihmc:mecano-yovariables:17-0.12.3")
 
    apiBytedecoNatives("javacpp")
