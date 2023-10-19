8a1a56a8edbcb92115b7339473c46ef3abea98ad
Beomyeong Park <bpark@ihmc.org>
2023-10-19 16:58:18 -0500

    Fixing the STP rendering for convexPolytope3D

--- scs2-examples/src/main/java/us/ihmc/scs2/examples/sessionVisualizer/jfx/STPConvexPolytope3DVisualizer.java
+++ scs2-examples/src/main/java/us/ihmc/scs2/examples/sessionVisualizer/jfx/STPConvexPolytope3DVisualizer.java
@@ -4,20 +4,30 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
-import us.ihmc.scs2.definition.geometry.STPConvexPolytope3DDefinition;
+import us.ihmc.euclid.shape.convexPolytope.ConvexPolytope3D;
+import us.ihmc.euclid.shape.convexPolytope.tools.EuclidPolytopeFactories;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.scs2.definition.visual.TriangleMesh3DFactories;
 import us.ihmc.scs2.sessionVisualizer.jfx.definition.JavaFXTriangleMesh3DDefinitionInterpreter;
+import us.ihmc.scs2.simulation.shapes.STPConvexPolytope3D;
 
 public class STPConvexPolytope3DVisualizer
 {
    public static void main(String[] args)
    {
-      STPConvexPolytope3DDefinition stpConvexPolytope3D = new STPConvexPolytope3DDefinition();
-//      ConvexPolytope3DDefinition polytope = EuclidPolytopeFactories.newIcosahedron(0.1);
-//      ConvexPolytope3DReadOnly convexPolytope new ConvexPolytope3DReadOnly();
-//      convexPolytope.getv
-//      polytope.getVertices().forEach(v -> stpConvexPolytope3D.addVertex(new Tuple3DProperty(v.getX(), v.getY(), v.getZ())));
-      stpConvexPolytope3D.setMargins(0.1, 0.5);
-      TriangleMesh mesh = JavaFXTriangleMesh3DDefinitionInterpreter.interpretDefinition(stpConvexPolytope3D);
+      STPConvexPolytope3D stpConvexPolytope3D = new STPConvexPolytope3D();
+      ConvexPolytope3D polytope = EuclidPolytopeFactories.newIcosahedron(0.3);
+      polytope.getVertices().forEach(v->stpConvexPolytope3D.addVertex(new Point3D(v.getX(), v.getY(), v.getZ())));
+
+
+      stpConvexPolytope3D.setMargins(0.01, 0.025);
+      double smallRadius = stpConvexPolytope3D.getSmallRadius();
+      double largeRadius = stpConvexPolytope3D.getLargeRadius();
+
+      TriangleMesh mesh = JavaFXTriangleMesh3DDefinitionInterpreter.interpretDefinition(TriangleMesh3DFactories.toSTPConvexPolytope3DMesh(stpConvexPolytope3D,
+                                                                                                                                          smallRadius,
+                                                                                                                                          largeRadius,
+                                                                                                                                          false));
       MeshView meshView = new MeshView(mesh);
       meshView.setMaterial(new PhongMaterial(Color.AQUAMARINE));
       Simple3DViewer.view3DObjects(meshView);
