9912c4e28465ca12075b9d987fc93bfe75c7ed7a
Beomyeong Park <bpark@ihmc.org>
2023-10-19 15:19:27 -0500

    Fixing the STP rendering using the STPBox3D not STPBox3Ddefinition

--- scs2-examples/src/main/java/us/ihmc/scs2/examples/sessionVisualizer/jfx/STPBox3DVisualizer.java
+++ scs2-examples/src/main/java/us/ihmc/scs2/examples/sessionVisualizer/jfx/STPBox3DVisualizer.java
@@ -2,38 +2,31 @@ package us.ihmc.scs2.examples.sessionVisualizer.jfx;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.TriangleMesh;
-import us.ihmc.scs2.definition.geometry.STPBox3DDefinition;
+import us.ihmc.scs2.definition.visual.TriangleMesh3DFactories;
 import us.ihmc.scs2.sessionVisualizer.jfx.definition.JavaFXTriangleMesh3DDefinitionInterpreter;
 import javafx.scene.shape.MeshView;
 import javafx.scene.paint.PhongMaterial;
+import us.ihmc.scs2.simulation.shapes.STPBox3D;
 
 public class STPBox3DVisualizer
 {
    public static void main(String[] args)
    {
-      STPBox3DDefinition stpBox3D = new STPBox3DDefinition(0.25, 0.25, 0.25);
+      STPBox3D stpBox3D = new STPBox3D(0.25, 0.25, 0.25);
       stpBox3D.setMargins(0.02, 0.05);
+      double smallRadius = stpBox3D.getSmallRadius();
+      double largeRadius = stpBox3D.getLargeRadius();
 
-      TriangleMesh mesh = JavaFXTriangleMesh3DDefinitionInterpreter.interpretDefinition(stpBox3D);
-//      TriangleMesh mesh = JavaFXTriangleMesh3DDefinitionInterpreter.interpretDefinition(TriangleMesh3DFactories.TriangleMesh(stpBox3D));
-////            null,
-////                                                                                                                               stpBox3D.get(),
-////                                                                                                                               stpBox3D.getSmallRadius(),
-////                                                                                                                               stpBox3D.getLargeRadius(),
-////                                                                                                                               false));
-//      mesh = JavaFXTriangleMesh3DDefinitionInterpreter.interpretDefinition(TriangleMesh3DFactories.toSTPBox3DMesh(stpBox3D),
-//                                                                           (stpBox3D.getSizeX(), stpBox3D.getSizeY(),stpBox3D.getSizeZ()),
-//      stpBox3D.sma)
+      TriangleMesh mesh = JavaFXTriangleMesh3DDefinitionInterpreter.interpretDefinition(TriangleMesh3DFactories.toSTPBox3DMesh(null,
+                                                                                                                               stpBox3D.getSize(),
+                                                                                                                               smallRadius,
+                                                                                                                               largeRadius,
+                                                                                                                               false));
+
       MeshView meshView = new MeshView(mesh);
       meshView.setMaterial(new PhongMaterial(Color.AQUAMARINE));
       Simple3DViewer.view3DObjects(meshView);
-//      Quaternion q = new Quaternion();
-//      q.appendPitchRotation(0.4);
-//      q.appendRollRotation(0.3);
-//      FramePose3D pose = new FramePose3D();
-//      STPBox3D stpBox3D = new STPBox3D(pose, 0.1, 0.2, 0.3);
-//      stpBox3D.setMargins(0.015,0.035);
-//      stpBox3D.get
+
 
 
    }
