1d63f8f902811bbb7da2d7288aa6e4b85cec8a33
Sylvain Bertrand <sbertrand@ihmc.org>
2023-10-25 22:19:31 -0500

    New SimRevoluteTwinsJoint.

---
+++ scs2-definition/src/main/java/us/ihmc/scs2/definition/robot/RevoluteTwinsJointDefinition.java
@@ -0,0 +1,225 @@
+package us.ihmc.scs2.definition.robot;
+
+import us.ihmc.euclid.tools.EuclidHashCodeTools;
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.mecano.multiBodySystem.RevoluteTwinsJoint;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
+import us.ihmc.scs2.definition.YawPitchRollTransformDefinition;
+
+import java.util.Objects;
+
+public class RevoluteTwinsJointDefinition extends OneDoFJointDefinition
+{
+   private String jointNameA;
+   private String jointNameB;
+   private YawPitchRollTransformDefinition transformAToPredecessor = new YawPitchRollTransformDefinition();
+   private YawPitchRollTransformDefinition transformBToA = new YawPitchRollTransformDefinition();
+   private RigidBodyDefinition bodyAB = new RigidBodyDefinition();
+
+   private int actuatedJointIndex;
+   private double constraintRatio;
+   private double constraintOffset;
+
+   public RevoluteTwinsJointDefinition()
+   {
+   }
+
+   public RevoluteTwinsJointDefinition(String name)
+   {
+      super(name);
+      bodyAB.setName(name + "_AB");
+   }
+
+   public RevoluteTwinsJointDefinition(String name, Vector3DReadOnly axis)
+   {
+      this(name);
+      setAxis(axis);
+   }
+
+   public void setJointNameA(String jointNameA)
+   {
+      this.jointNameA = jointNameA;
+   }
+
+   public void setJointNameB(String jointNameB)
+   {
+      this.jointNameB = jointNameB;
+   }
+
+   public void setJointNames(String jointNameA, String jointNameB)
+   {
+      this.jointNameA = jointNameA;
+      this.jointNameB = jointNameB;
+   }
+
+   public void setTransformAToPredecessor(YawPitchRollTransformDefinition transformAToPredecessor)
+   {
+      this.transformAToPredecessor = transformAToPredecessor;
+   }
+
+   public void setTransformAToPredecessor(RigidBodyTransformReadOnly transformAToPredecessor)
+   {
+      this.transformAToPredecessor.set(transformAToPredecessor);
+   }
+
+   public void setTransformBToA(YawPitchRollTransformDefinition transformBToA)
+   {
+      this.transformBToA = transformBToA;
+   }
+
+   public void setTransformBToA(RigidBodyTransformReadOnly transformBToA)
+   {
+      this.transformBToA.set(transformBToA);
+   }
+
+   public void setJointTransforms(YawPitchRollTransformDefinition transformAToPredecessor, YawPitchRollTransformDefinition transformBToA)
+   {
+      this.transformAToPredecessor = transformAToPredecessor;
+      this.transformBToA = transformBToA;
+   }
+
+   public void setJointTransforms(RigidBodyTransformReadOnly transformAToPredecessor, RigidBodyTransformReadOnly transformBToA)
+   {
+      this.transformAToPredecessor.set(transformAToPredecessor);
+      this.transformBToA.set(transformBToA);
+   }
+
+   public void setBodyAB(RigidBodyDefinition bodyAB)
+   {
+      this.bodyAB = bodyAB;
+   }
+
+   public void setActuatedJointIndex(int actuatedJointIndex)
+   {
+      this.actuatedJointIndex = actuatedJointIndex;
+   }
+
+   public void setConstraintRatio(double constraintRatio)
+   {
+      this.constraintRatio = constraintRatio;
+   }
+
+   public void setConstraintOffset(double constraintOffset)
+   {
+      this.constraintOffset = constraintOffset;
+   }
+
+   public String getJointNameA()
+   {
+      return jointNameA;
+   }
+
+   public String getJointNameB()
+   {
+      return jointNameB;
+   }
+
+   public YawPitchRollTransformDefinition getTransformAToPredecessor()
+   {
+      return transformAToPredecessor;
+   }
+
+   public YawPitchRollTransformDefinition getTransformBToA()
+   {
+      return transformBToA;
+   }
+
+   public RigidBodyDefinition getBodyAB()
+   {
+      return bodyAB;
+   }
+
+   public int getActuatedJointIndex()
+   {
+      return actuatedJointIndex;
+   }
+
+   public double getConstraintRatio()
+   {
+      return constraintRatio;
+   }
+
+   public double getConstraintOffset()
+   {
+      return constraintOffset;
+   }
+
+   @Override
+   public RevoluteTwinsJoint toJoint(RigidBodyBasics predecessor)
+   {
+      return new RevoluteTwinsJoint(getName(),
+                                    predecessor,
+                                    jointNameA,
+                                    jointNameB,
+                                    bodyAB.getName(),
+                                    transformAToPredecessor,
+                                    transformBToA,
+                                    bodyAB.getMomentOfInertia(),
+                                    bodyAB.getMass(),
+                                    bodyAB.getInertiaPose(),
+                                    actuatedJointIndex,
+                                    constraintRatio,
+                                    constraintOffset,
+                                    getAxis());
+   }
+
+   @Override
+   public RevoluteTwinsJointDefinition copy()
+   {
+      RevoluteTwinsJointDefinition clone = new RevoluteTwinsJointDefinition(getName(), getAxis());
+      clone.jointNameA = jointNameA;
+      clone.jointNameB = jointNameB;
+      clone.transformAToPredecessor.set(transformAToPredecessor);
+      clone.transformBToA.set(transformBToA);
+      clone.bodyAB = bodyAB.copy();
+      clone.actuatedJointIndex = actuatedJointIndex;
+      clone.constraintRatio = constraintRatio;
+      clone.constraintOffset = constraintOffset;
+      return clone;
+   }
+
+   @Override
+   public int hashCode()
+   {
+      long bits = super.hashCode();
+      bits = EuclidHashCodeTools.addToHashCode(bits, jointNameA);
+      bits = EuclidHashCodeTools.addToHashCode(bits, jointNameB);
+      bits = EuclidHashCodeTools.addToHashCode(bits, transformAToPredecessor);
+      bits = EuclidHashCodeTools.addToHashCode(bits, transformBToA);
+      bits = EuclidHashCodeTools.addToHashCode(bits, bodyAB);
+      bits = EuclidHashCodeTools.addToHashCode(bits, actuatedJointIndex);
+      bits = EuclidHashCodeTools.addToHashCode(bits, constraintRatio);
+      bits = EuclidHashCodeTools.addToHashCode(bits, constraintOffset);
+      return EuclidHashCodeTools.toIntHashCode(bits);
+   }
+
+   @Override
+   public boolean equals(Object object)
+   {
+      if (object == this)
+         return true;
+      if (!super.equals(object))
+         return false;
+
+      RevoluteTwinsJointDefinition other = (RevoluteTwinsJointDefinition) object;
+
+      if (!Objects.equals(jointNameA, other.jointNameA))
+         return false;
+      if (!Objects.equals(jointNameB, other.jointNameB))
+         return false;
+      if (!Objects.equals(transformAToPredecessor, other.transformAToPredecessor))
+         return false;
+      if (!Objects.equals(transformBToA, other.transformBToA))
+         return false;
+      if (!Objects.equals(bodyAB, other.bodyAB))
+         return false;
+      if (actuatedJointIndex != other.actuatedJointIndex)
+         return false;
+      if (Double.doubleToLongBits(constraintRatio) != Double.doubleToLongBits(other.constraintRatio))
+         return false;
+      if (Double.doubleToLongBits(constraintOffset) != Double.doubleToLongBits(other.constraintOffset))
+         return false;
+      return true;
+   }
+}
---
+++ scs2-simulation/src/main/java/us/ihmc/scs2/simulation/robot/multiBodySystem/SimRevoluteTwinsJoint.java
@@ -0,0 +1,219 @@
+package us.ihmc.scs2.simulation.robot.multiBodySystem;
+
+import us.ihmc.euclid.matrix.interfaces.Matrix3DReadOnly;
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.RevoluteJointBasics;
+import us.ihmc.mecano.multiBodySystem.interfaces.RevoluteTwinsJointBasics;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
+import us.ihmc.mecano.spatial.interfaces.TwistReadOnly;
+import us.ihmc.mecano.tools.MecanoFactories;
+import us.ihmc.mecano.yoVariables.multiBodySystem.YoRevoluteTwinsJoint;
+import us.ihmc.scs2.definition.robot.RevoluteTwinsJointDefinition;
+import us.ihmc.scs2.simulation.robot.SimJointAuxiliaryData;
+import us.ihmc.scs2.simulation.robot.multiBodySystem.interfaces.SimOneDoFJointBasics;
+import us.ihmc.scs2.simulation.robot.multiBodySystem.interfaces.SimRigidBodyBasics;
+import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoBoolean;
+import us.ihmc.yoVariables.variable.YoDouble;
+
+public class SimRevoluteTwinsJoint extends YoRevoluteTwinsJoint implements SimOneDoFJointBasics, RevoluteTwinsJointBasics
+{
+   private final YoRegistry registry;
+   private final SimJointAuxiliaryData auxiliaryData;
+   private final TwistReadOnly jointDeltaTwist;
+   private final YoDouble deltaQd;
+   private final YoBoolean isPinned;
+   private final YoDouble damping;
+
+   public SimRevoluteTwinsJoint(RevoluteTwinsJointDefinition definition, SimRigidBodyBasics predecessor)
+   {
+      this(definition.getName(),
+           predecessor,
+           definition.getJointNameA(),
+           definition.getJointNameB(),
+           definition.getBodyAB().getName(),
+           definition.getTransformAToPredecessor(),
+           definition.getTransformBToA(),
+           definition.getBodyAB().getMomentOfInertia(),
+           definition.getBodyAB().getMass(),
+           definition.getBodyAB().getInertiaPose(),
+           definition.getActuatedJointIndex(),
+           definition.getConstraintRatio(),
+           definition.getConstraintOffset(),
+           definition.getAxis());
+      setJointLimits(definition.getPositionLowerLimit(), definition.getPositionUpperLimit());
+      setVelocityLimits(definition.getVelocityLowerLimit(), definition.getVelocityUpperLimit());
+      setEffortLimits(definition.getEffortLowerLimit(), definition.getEffortUpperLimit());
+      setDamping(definition.getDamping());
+   }
+
+   public SimRevoluteTwinsJoint(String name,
+                                SimRigidBodyBasics predecessor,
+                                String jointNameA,
+                                String jointNameB,
+                                String bodyNameAB,
+                                RigidBodyTransformReadOnly transformAToPredecessor,
+                                RigidBodyTransformReadOnly transformBToA,
+                                Matrix3DReadOnly bodyInertiaAB,
+                                double bodyMassAB,
+                                RigidBodyTransformReadOnly bodyInertiaPoseAB,
+                                int actuatedJointIndex,
+                                double constraintRatio,
+                                double constraintOffset,
+                                Vector3DReadOnly jointAxis)
+   {
+      super(name,
+            predecessor,
+            jointNameA,
+            jointNameB,
+            bodyNameAB,
+            transformAToPredecessor,
+            transformBToA,
+            bodyInertiaAB,
+            bodyMassAB,
+            bodyInertiaPoseAB,
+            actuatedJointIndex,
+            constraintRatio,
+            constraintOffset,
+            jointAxis,
+            predecessor.getRegistry());
+
+      this.registry = predecessor.getRegistry();
+      auxiliaryData = new SimJointAuxiliaryData(this);
+      deltaQd = new YoDouble("qd_delta_" + getName(), registry);
+      jointDeltaTwist = MecanoFactories.newTwistReadOnly(this::getDeltaQd, getUnitJointTwist());
+      isPinned = new YoBoolean("is_" + getName() + "_pinned", registry);
+      damping = new YoDouble("damping_" + getName(), registry);
+   }
+
+   @Override
+   public YoRegistry getRegistry()
+   {
+      return registry;
+   }
+
+   @Override
+   public SimJointAuxiliaryData getAuxiliaryData()
+   {
+      return auxiliaryData;
+   }
+
+   @Override
+   public void setSuccessor(RigidBodyBasics successor)
+   {
+      if (successor instanceof SimRigidBodyBasics)
+         super.setSuccessor(successor);
+      else
+         throw new IllegalArgumentException("Can only set a " + SimRigidBodyBasics.class.getSimpleName() + " as successor of a " + getClass().getSimpleName());
+   }
+
+   @Override
+   public SimRigidBodyBasics getPredecessor()
+   {
+      return (SimRigidBodyBasics) super.getPredecessor();
+   }
+
+   @Override
+   public SimRigidBodyBasics getSuccessor()
+   {
+      return (SimRigidBodyBasics) super.getSuccessor();
+   }
+
+   @Override
+   public void resetState()
+   {
+      SimOneDoFJointBasics.super.resetState();
+      updateFrame();
+   }
+
+   @Override
+   public void setQ(double q)
+   {
+      if (!Double.isFinite(q))
+         throw new IllegalStateException("Invalid joint configuration: " + q);
+
+      super.setQ(q);
+   }
+
+   @Override
+   public double computeActuatedJointQ(double q)
+   {
+      if (!Double.isFinite(q))
+         throw new IllegalStateException("Invalid joint configuration: " + q);
+
+      double actuatedJointQ = super.computeActuatedJointQ(q);
+
+      if (!Double.isFinite(actuatedJointQ))
+         throw new IllegalStateException("Invalid joint configuration: " + actuatedJointQ);
+
+      return actuatedJointQ;
+   }
+
+   @Override
+   public double computeActuatedJointQdd(double qdd)
+   {
+      if (!Double.isFinite(qdd))
+         throw new IllegalStateException("Invalid joint acceleration: " + qdd);
+
+      double actuatedJointQdd = super.computeActuatedJointQdd(qdd);
+
+      if (!Double.isFinite(actuatedJointQdd))
+         throw new IllegalStateException("Invalid joint acceleration: " + actuatedJointQdd);
+
+      return actuatedJointQdd;
+   }
+
+   @Override
+   public double getDamping()
+   {
+      return damping.getValue();
+   }
+
+   @Override
+   public void setDamping(double damping)
+   {
+      this.damping.set(damping);
+   }
+
+   @Override
+   public double getDeltaQd()
+   {
+      return deltaQd.getValue();
+   }
+
+   @Override
+   public void setDeltaQd(double deltaQd)
+   {
+      this.deltaQd.set(deltaQd);
+   }
+
+   @Override
+   public void setJointAngularDeltaVelocity(Vector3DReadOnly jointAngularDeltaVelocity)
+   {
+      setDeltaQd(getJointAxis().dot(jointAngularDeltaVelocity));
+   }
+
+   @Override
+   public void setJointLinearDeltaVelocity(Vector3DReadOnly jointLinearDeltaVelocity)
+   {
+   }
+
+   @Override
+   public TwistReadOnly getJointDeltaTwist()
+   {
+      return jointDeltaTwist;
+   }
+
+   @Override
+   public void setPinned(boolean isPinned)
+   {
+      this.isPinned.set(isPinned);
+   }
+
+   @Override
+   public boolean isPinned()
+   {
+      return isPinned.getValue();
+   }
+}
