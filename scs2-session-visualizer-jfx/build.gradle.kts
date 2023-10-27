184f7913b9eb0a6ecdf8b0958fc0b95348f84a55
Sylvain Bertrand <sbertrand@ihmc.org>
2023-10-27 12:17:32 -0500

    :arrow_up: ihmc-graphics-description 0.20.7

--- scs2-session-visualizer-jfx/build.gradle.kts
+++ scs2-session-visualizer-jfx/build.gradle.kts
@@ -27,7 +27,7 @@ mainDependencies {
    api("us.ihmc:euclid:0.20.0")
    api("us.ihmc:euclid-shape:0.20.0")
    api("us.ihmc:euclid-frame:0.20.0")
-   api("us.ihmc:ihmc-graphics-description:0.20.6")
+   api("us.ihmc:ihmc-graphics-description:0.20.7")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:svgloader:0.0")
    api("us.ihmc:ihmc-javafx-extensions:17-0.2.1")
