cc5aff3f658561158f33e47127e4bf904ea39742
Sylvain Bertrand <sbertrand@ihmc.org>
2023-12-11 13:48:29 -0600

    :arrow_up: euclid 0.21.0

--- scs2-symbolic/build.gradle.kts
+++ scs2-symbolic/build.gradle.kts
@@ -15,7 +15,7 @@ mainDependencies {
    api("us.ihmc:scs2-definition:source")
    api("us.ihmc:scs2-shared-memory:source")
 
-   api("us.ihmc:euclid:0.20.0")
+   api("us.ihmc:euclid:0.21.0")
    api("us.ihmc:euclid-frame:0.20.0")
    api("us.ihmc:ihmc-yovariables:0.10.0")
 }
