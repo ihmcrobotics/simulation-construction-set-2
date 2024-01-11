9bdbad7e8b1245035b9d10afcdcd0ed9961105f2
Sylvain <sbertrand@ihmc.org>
2024-01-11 11:15:27 +0100

    Added simple test for parsing the revolute twins URDF.

---
+++ scs2-examples/src/test/java/us/ihmc/scs2/examples/urdf/SimpleRevoluteTwinsRobotTest.java
@@ -0,0 +1,60 @@
+package us.ihmc.scs2.examples.urdf;
+
+import org.junit.jupiter.api.Test;
+import us.ihmc.euclid.Axis3D;
+import us.ihmc.euclid.tools.EuclidCoreTestTools;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.scs2.definition.YawPitchRollTransformDefinition;
+import us.ihmc.scs2.definition.robot.MomentOfInertiaDefinition;
+import us.ihmc.scs2.definition.robot.RevoluteTwinsJointDefinition;
+import us.ihmc.scs2.definition.robot.RigidBodyDefinition;
+import us.ihmc.scs2.definition.robot.RobotDefinition;
+import us.ihmc.scs2.definition.robot.urdf.URDFTools;
+import us.ihmc.scs2.definition.robot.urdf.items.URDFModel;
+
+import java.io.InputStream;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleRevoluteTwinsRobotTest
+{
+   @Test
+   public void testLoadingURDF() throws Exception
+   {
+      InputStream is = SimpleCrossFourBarURDFRobot.class.getClassLoader().getResourceAsStream("urdf/SimpleRevoluteTwinsRobot.urdf");
+      URDFModel urdfModel = URDFTools.loadURDFModel(is, Collections.emptyList(), SimpleCrossFourBarURDFRobot.class.getClassLoader());
+      URDFTools.URDFParserProperties parserProperties = new URDFTools.URDFParserProperties();
+      parserProperties.setRootJointFactory(null);
+      RobotDefinition robotDefinition = URDFTools.toRobotDefinition(urdfModel, parserProperties);
+      assertAsExpected(robotDefinition);
+   }
+
+   /**
+    * Asserts that the robot definition is as expected.
+    *
+    * @param robotDefinition the robot definition to verify.
+    */
+   private static void assertAsExpected(RobotDefinition robotDefinition)
+   {
+      RigidBodyDefinition rootBody = robotDefinition.getRootBodyDefinition();
+      assertEquals("baseLink", rootBody.getName());
+      assertEquals(1, rootBody.getChildrenJoints().size());
+      assertEquals(RevoluteTwinsJointDefinition.class, rootBody.getChildrenJoints().get(0).getClass());
+
+      RevoluteTwinsJointDefinition joint = (RevoluteTwinsJointDefinition) rootBody.getChildrenJoints().get(0);
+      assertEquals("revoluteTwins", joint.getName());
+      EuclidCoreTestTools.assertEquals(Axis3D.Y, joint.getAxis(), 0.0);
+
+      assertEquals("jointA", joint.getJointNameA());
+      assertEquals("jointB", joint.getJointNameB());
+
+      EuclidCoreTestTools.assertEquals(new YawPitchRollTransformDefinition(new Point3D(0.1, 0, 0.2)), joint.getTransformAToPredecessor(), 0.0);
+      EuclidCoreTestTools.assertEquals(new YawPitchRollTransformDefinition(new Point3D(0.0, 0, 0.2)), joint.getTransformBToA(), 0.0);
+
+      RigidBodyDefinition bodyAB = joint.getBodyAB();
+      assertEquals("bodyAB", bodyAB.getName());
+      assertEquals(0.025, bodyAB.getMass());
+      EuclidCoreTestTools.assertEquals(new MomentOfInertiaDefinition(1.0e-8, 1.0e-8, 1.0e-8), bodyAB.getMomentOfInertia(), 0.0);
+   }
+}
