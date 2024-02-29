ff4f350da7f417317074bb5746b276065e83f9bc
Sylvain Bertrand <sbertrand@ihmc.org>
2024-02-29 16:17:56 -0600

    :arrow_up: mecano 17-0.18.1

--- scs2-definition/build.gradle.kts
+++ scs2-definition/build.gradle.kts
@@ -16,7 +16,7 @@ mainDependencies {
    api("us.ihmc:euclid-frame:0.21.0")
    api("us.ihmc:ihmc-commons:0.32.0")
    api("us.ihmc:ihmc-yovariables:0.11.1")
-   api("us.ihmc:mecano:17-0.18.0")
+   api("us.ihmc:mecano:17-0.18.1")
 }
 
 testDependencies {
