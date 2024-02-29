227de3c6e810f4021a1763dcc664140163861580
Sylvain Bertrand <sbertrand@ihmc.org>
2024-02-29 17:01:56 -0600

    :arrow_up: ihmc-robot-data-logger 0.28.9

--- scs2-session-logger/build.gradle.kts
+++ scs2-session-logger/build.gradle.kts
@@ -14,7 +14,7 @@ mainDependencies {
    api("us.ihmc:scs2-session:source")
    api("us.ihmc:scs2-simulation:source") // TODO Need to fix this, it needs the Robot.
 
-   api("us.ihmc:ihmc-robot-data-logger:0.28.8")
+   api("us.ihmc:ihmc-robot-data-logger:0.28.9")
    api("com.github.luben:zstd-jni:1.5.5-10")
    api("org.antlr:antlr4-runtime:4.13.1")
    //api("org.lz4:lz4-java:1.8.0")
