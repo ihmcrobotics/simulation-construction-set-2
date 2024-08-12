1c83db652234125780ef67dc841b0b79860b48f4
nkitchel <nkitchel@ihmc.org>
2024-08-12 10:03:05 -0500

    Revert ":arrow_up: ihmc-yovariables 0.12.1"
    This reverts commit 118e31a397db74a109e5b382de44e91ba6b8b59a.

--- scs2-symbolic/build.gradle.kts
+++ scs2-symbolic/build.gradle.kts
@@ -16,7 +16,7 @@ mainDependencies {
 
    api("us.ihmc:euclid:0.21.0")
    api("us.ihmc:euclid-frame:0.21.0")
-   api("us.ihmc:ihmc-yovariables:0.12.1")
+   api("us.ihmc:ihmc-yovariables:0.12.0")
 }
 
 testDependencies {
